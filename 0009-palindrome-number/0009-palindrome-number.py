bac29612f1ad64b6d599d93632a5b42f6594c94f
Nikossik <61514022+Nikossik@users.noreply.github.com>
2024-09-04 14:52:52 +0300

    Time: 27 ms (94.49%), Space: 11.4 MB (92.93%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if str(x)[0] == '-':
+            return False
+        if str(x) == str(x)[::-1]:
+            return True
+        else: return False
+
+        
